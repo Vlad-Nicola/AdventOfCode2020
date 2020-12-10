8a88dbe18f1355af9fe688512ca97f2fccc1f413
Vlad Nicola <vladmihai.nicola@yahoo.com>
2020-12-10 13:22:44 +0200

    Day9 Fix!

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -5,12 +5,12 @@
 
 constexpr unsigned int cache_size = 25;
 
-unsigned int findEncriptionWeakness(const std::list<unsigned int>& number_list, const unsigned int invalid_number)
+unsigned long long findEncriptionWeakness(const std::list<unsigned long long>& number_list, const unsigned long long invalid_number)
 {
     auto it_first = number_list.crbegin();
     auto it_second = std::next(it_first);
 
-    unsigned long long sum = static_cast<unsigned long long>(*it_first) + *it_second;
+    unsigned long long sum = *it_first + *it_second;
     for ( ; it_first != number_list.crend(); sum -= *it_first, ++it_first)
     {
         bool unchanged_it_second = true;
@@ -22,15 +22,15 @@ unsigned int findEncriptionWeakness(const std::list<unsigned int>& number_list, 
 
         for ( ; it_second != number_list.crend(); ++it_second)
         {
-            /*if (!unchanged_it_second)
+            if (!unchanged_it_second)
             {
                 sum += *it_second;
-            }*/
+            }
 
             if (sum == invalid_number)
             {
-                unsigned int min = *std::min_element(it_first, std::next(it_second));
-                unsigned int max = *std::max_element(it_first, std::next(it_second));
+                unsigned long long min = *std::min_element(it_first, std::next(it_second));
+                unsigned long long max = *std::max_element(it_first, std::next(it_second));
                 return min + max;
             }
             else if (sum > invalid_number)
@@ -44,12 +44,12 @@ unsigned int findEncriptionWeakness(const std::list<unsigned int>& number_list, 
     return 0;
 }
 
-bool checkValue(const std::list<unsigned int>& numbers_list, const unsigned int val)
+bool checkValue(const std::list<unsigned long long>& numbers_list, const unsigned long long val)
 {
     unsigned int i{ cache_size };
     for (auto it_first = numbers_list.crbegin(); i != 0; --i)
     {
-        unsigned int val_first = *it_first;
+        unsigned long long val_first = *it_first;
         ++it_first;
         unsigned int j{ i - 1 };
         for (auto it_second = it_first; j != 0; --j, ++it_second)
@@ -68,17 +68,17 @@ int main()
 {
     std::ifstream file("input.txt");
     
-    std::list<unsigned int> numbers_list;
-    unsigned int invalid_value = 0;
+    std::list<unsigned long long> numbers_list;
+    unsigned long long invalid_value = 0;
 
-    for (unsigned int val, i{ cache_size }; file >> val && i != 0; --i)
+    for (unsigned long long val, i{ cache_size }; file >> val && i != 0; --i)
     {
             numbers_list.push_back(val);
     }
 
     auto it = numbers_list.cbegin();
 
-    for (unsigned int val; file >> val; )
+    for (unsigned long long val; file >> val; )
     {
         if (!checkValue(numbers_list, val))
         {
@@ -97,3 +97,5 @@ int main()
     return 0;
 }
 
+//(Part 1) First invalid value: 1492208709
+//(Part 2) Encription weakness: 238243506
