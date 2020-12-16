2e274bc6aad910cbe867ecdd421be2fceb1745d0
Vlad Nicola <vladmihai.nicola@yahoo.com>
2020-12-16 04:29:15 +0200

    Cut code length for Day15

--- Day15/Day15.cpp
+++ Day15/Day15.cpp
@@ -3,34 +3,16 @@
 
 int main()
 {
-    std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> spoken_catalog
-    {
-        {13, {0, 1}},
-        {0 , {0, 2}},
-        {10, {0, 3}},
-        {12, {0, 4}},
-        {1 , {0, 5}},
-        {5 , {0, 6}}    //Last input is last_spoken
-    };
+    std::unordered_map<unsigned int, unsigned int> spoken_catalog {{13, 1}, {0, 2}, {10, 3}, {12, 4}, {1, 5}, {5, 6}};
 
     unsigned int last_spoken{ 8 }, last_spoken_turn_2020{};
-
     for (unsigned int turn{ spoken_catalog.size() + 1};  turn != 30000000; ++turn)
     {
         if (2020 == turn) last_spoken_turn_2020 = last_spoken;
 
-        const auto& [it, inserted] = spoken_catalog.insert({ last_spoken, { 0, turn } });
-        if (inserted)
-        {
-            last_spoken = 0;
-        }
-        else
-        {
-            auto& [far, close] = it->second;
-            far = close;
-            close = turn;
-            last_spoken = close - far;
-        }
+        const auto& [it, inserted] = spoken_catalog.insert({ last_spoken, turn });
+        last_spoken = inserted ? 0 : turn - it->second;
+        it->second = turn;
     }
     
     //Part1
